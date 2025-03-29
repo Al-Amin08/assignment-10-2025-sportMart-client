49d07b53f054679f9a0b6f8e1ac94c0b1016aae6
Al-Amin08 <mehadislam08@gmail.com>
2025-03-30 00:58:35 +0600

    add delete functionality in my equipment

--- src/Component/MyEquipment.jsx
+++ src/Component/MyEquipment.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyEquipment = () => {
   const loadedData = useLoaderData();
+  const [loadedProducts, setLoadedProducts] = useState(loadedData);
   //   const {
   //     item,
   //     category,
@@ -17,10 +19,43 @@ const MyEquipment = () => {
   //     photo,
   //   } = loadedData;
   console.log(loadedData);
+
+  const handleDelete = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/equipments/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+            console.log(data);
+          });
+        const remainingProducts = loadedProducts.filter(
+          (product) => product._id != _id
+        );
+        setLoadedProducts(remainingProducts);
+      }
+    });
+  };
   return (
     <div className="w-10/12 mx-auto ">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {loadedData.map((product) => (
+        {loadedProducts.map((product) => (
           <div className="card bg-base-100  shadow-sm">
             <figure className="px-10 pt-10">
               <img
@@ -37,7 +72,12 @@ const MyEquipment = () => {
               </p>
               <div className="card-actions pt-5">
                 <button className="btn btn-primary">Update</button>
-                <button className="btn btn-primary">Delete</button>
+                <button
+                  onClick={() => handleDelete(product._id)}
+                  className="btn btn-primary"
+                >
+                  Delete
+                </button>
               </div>
             </div>
           </div>
