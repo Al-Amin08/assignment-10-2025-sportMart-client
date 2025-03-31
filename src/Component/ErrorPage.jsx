ff5ed229d0e35c7aa115d97a5e4575f82a8254fd
Al-Amin08 <mehadislam08@gmail.com>
2025-03-31 22:23:14 +0600

    error page added

--- src/Component/ErrorPage.jsx
+++ src/Component/ErrorPage.jsx
@@ -1,7 +1,16 @@
+import Lottie from "lottie-react";
 import React from "react";
+import error from "../assets/Error.json";
 
 const ErrorPage = () => {
-  return <div>Error page</div>;
+  return (
+    // <div className="  overflow-hidden ">
+    //   <Lottie className="" animationData={bird} loop={true} />
+    // </div>
+    <div className="w-screen h-screen overflow-hidden">
+      <Lottie className="w-full h-full" animationData={error} loop />
+    </div>
+  );
 };
 
 export default ErrorPage;
